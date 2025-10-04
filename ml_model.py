ec2149c151076eefcd72ec68488585604108d1f8
Matias Freire <matiaspfreire@gmail.com>
2025-10-04 12:17:12 -0400

    Model.pkl Uploaded(modelSaved) + Ml_model.py was updated)

--- ml_model.py
+++ ml_model.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import joblib
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -16,7 +17,6 @@ url = "https://raw.githubusercontent.com/MatiasPF1/KOI-Dataset/main/NasaDataset.
 df = pd.read_csv(url)
 pd.set_option('display.max_columns', None)  # show all columns
 
-#df.shape (9564, 49) 9564 training samples, 49 collumns of features
 
 '''
 Based on the Research article: 
@@ -26,34 +26,35 @@ Assessment of Ensemble-Based Machine Learning Algorithms for Exoplanet Identific
 https://doi.org/10.3390/electronics13193950
 '''
 
-#Dropping collumns
-df.drop(['kepid','kepoi_name','kepler_name','koi_pdisposition','koi_score','koi_teq_err1','koi_teq_err2'], axis=1, inplace=True)
-'''
-drop(): will drop collumns
-      Arguments
-      1-['']: whatever collumn we want to drop
-      2-axis: x(0) or y(1) axis that want to be dropped
-      3-inplace modifies original dataFrame
-'''
-df.head(5)
-#df.shape (9564, 42)
-#Exploring koi_tce_delivname
-df['koi_tce_delivname'].value_counts()  #Getting how many categories and frequency
-df['koi_tce_delivname'].isna().sum() #sum of Nan values
-df = df.dropna(subset=['koi_tce_delivname']) #Dropping rows where the Koi_tce_delivname has nan values
+                                        #1-Dropping collumns(3 Parts)
+df.drop(['kepid','kepoi_name','kepler_name','koi_pdisposition','koi_score','koi_teq_err1','koi_teq_err2'], axis=1, inplace=True) #1st Drop
 
-#df.shape 334 rows eliminated, all clean
-#df['koi_disposition'].value_counts()  Getting how many categories and frequency
-df= df[df["koi_disposition"] != "FALSE POSITIVE"] #Dropping FALSE POSITIVE rows
-#df['koi_disposition'].value_counts() confirmed 2735 candidate 1919
+df.drop(['koi_tce_delivname','ra','koi_time0bk','dec'], axis=1, inplace=True) # 2nd Drop
 
+df.drop(columns=['koi_period_err1','koi_period_err2','koi_duration_err1','koi_duration_err2',
+                 'koi_depth_err1','koi_depth_err2','koi_prad_err1','koi_prad_err2',
+                 'koi_insol_err1','koi_insol_err2','koi_impact_err1','koi_impact_err2',
+                 'koi_steff_err1','koi_steff_err2','koi_slogg_err1','koi_slogg_err2',
+                 'koi_srad_err1','koi_srad_err2'],axis=1, inplace=True) # 3rd Drop
+
+
+                                        #3-Dropping False Positive rows
+df= df[df["koi_disposition"] != "FALSE POSITIVE"] 
+
+                                        #4-Mapping Confirmed/Candidate to Binary
 mapping = {'CONFIRMED': '0', 'CANDIDATE': '1'}
 df['koi_disposition'] = df['koi_disposition'].replace(mapping)
 
-#1- Getting X/Y Features
+                                        #5-Getting X/Y Features
 X = df.drop(columns=['koi_disposition']).values
 Y = df['koi_disposition'].values
 
+                                       #6-Setting Training/Testing
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=50, shuffle=True)
+
+
+
+                                       #7-PipeLine 
 imputer = SimpleImputer(strategy='mean')
 pipeline = Pipeline([
     ('imputer', imputer),  #Seems like there are some other NaNs
@@ -61,13 +62,14 @@ pipeline = Pipeline([
     ('adaboost', AdaBoostClassifier(n_estimators=974, learning_rate=0.11, random_state=50))
 ])
 
-# Stratified 10-fold cross-validation - Testing how the Model would probably gonna work in an last performance
-skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=50)
-cv_scores = cross_val_score(pipeline, X_train, y_train, cv=skf, scoring='accuracy')
-print("Cross-validation accuracy for each fold:", cv_scores)
-print("Mean cross-validation accuracy:", np.mean(cv_scores))
-
+                                        #9-Training 
 #Train actual Pipeline
 pipeline.fit(X_train, y_train)
 test_accuracy = pipeline.score(X_test, y_test)
 print("Test set accuracy:", test_accuracy)
+
+                                        #10-Saving Model
+# Save Model
+joblib.dump(pipeline, 'model.pkl')
+# Later: load it back
+model = joblib.load('model.pkl') #Saved as model.pkl , we will use this model 
